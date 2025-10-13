4ce19d83112d0f958aebf6af433d457d0633d11d
alejandrogoscu <alejandrogoscu@gmail.com>
2025-10-13 17:28:44 +0200

    feat(shared): initialize shared package with build config

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Barrel export para el paquete @pomodorise/shared
+ *
+ * Exporta todos los tipos, interfaces y utilidades compartidas
+ *
+ * Teacher note:
+ * - Este archivo actúa como punto de entrada único
+ * - Simplifica imports: `import { IUser } from '@pomodorise/shared'`
+ * - Em lugar de: `import { IUser } from '@pomodorise/shared/types'`
+ */
+
+// Aquí exportaremos tipos y utils en las siguientes micro-subfases
+export {};
